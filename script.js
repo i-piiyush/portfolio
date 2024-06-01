0772bc9238df6a92a507971d0219a8bcb6428676
i-piiyush <piyushchhabra3578@gmail.com>
2024-06-01 14:48:16 +0530

    comitted

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ gsap.from(".upper .right a",{
     delay:.2,
     stagger:0.2
 })
+
+const btn = document.querySelector(".btn")
+console.log(btn);
+function sendMessage() {
+    // Get the value of the input field
+    const message = document.getElementById('message').value;
+
+    // Encode the message for URL
+    const encodedMessage = encodeURIComponent(message);
+
+    const phoneNumber = '+918847654718';
+
+    // Construct the WhatsApp URL
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+    // Open the WhatsApp URL in a new window/tab
+    window.open(whatsappURL, '_blank');
+}
+btn.addEventListener("click",sendMessage)
